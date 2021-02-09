0866f4eb5f9886a26429d63464b7feca145a98e1
Felix Redondo <frmanzanares@hotmail.com>
2021-02-09 01:35:33 +0100

    Ejercicio 1.6 Introducción al useState

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css';
+
+const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+
+	return (
+		<div>
+			<div>
+				<h1>Give feedback</h1>
+				<button onClick={() => setGood(good + 1)}>Good</button>
+				<button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+				<button onClick={() => setBad(bad + 1)}>Bad</button>
+			</div>
+			<br/>
+			<div>
+				<h1>Statistics</h1>
+				<p><span>Good {good}</span></p>
+				<p><span>Neutral {neutral}</span></p>
+				<p><span>Bad {bad}</span></p>
+			</div>
+		</div>
+	)
+}
+
+ReactDOM.render(<App/>,
+	document.getElementById('root')
+)
