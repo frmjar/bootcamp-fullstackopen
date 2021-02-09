150c9ff4f40143a5c8cccfd04fe07389e5fbbfd5
Felix Redondo <frmanzanares@hotmail.com>
2021-02-09 01:48:20 +0100

    Ejercicio 1.8 Se crea el componente estadísticas. Se refactoriza el código

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,15 +2,29 @@ import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
 
+const Statistics = ({good, neutral, bad}) => {
+	const total = good + neutral + bad
+	const positive = (good / total) * 100
+	const average = (good - bad) / total
+
+	return (
+		<div>
+			<h1>Statistics</h1>
+			<p><span>Good: {good}</span></p>
+			<p><span>Neutral: {neutral}</span></p>
+			<p><span>Bad: {bad}</span></p>
+			<p><span>All: {total}</span></p>
+			<p><span>Average: {average}</span></p>
+			<p><span>Positive: {positive} %</span></p>
+		</div>
+	)
+}
+
 const App = () => {
 	// save clicks of each button to its own state
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
-
-	const total = good + neutral + bad
-	const positive = (good / total) * 100
-	const average = (good - bad) / total
 
 	return (
 		<div>
@@ -21,15 +35,7 @@ const App = () => {
 				<button onClick={() => setBad(bad + 1)}>Bad</button>
 			</div>
 			<br/>
-			<div>
-				<h1>Statistics</h1>
-				<p><span>Good: {good}</span></p>
-				<p><span>Neutral: {neutral}</span></p>
-				<p><span>Bad: {bad}</span></p>
-				<p><span>All: {total}</span></p>
-				<p><span>Average: {average}</span></p>
-				<p><span>Positive: {positive} %</span></p>
-			</div>
+			<Statistics good={good} neutral={neutral} bad={bad}/>
 		</div>
 	)
 }
