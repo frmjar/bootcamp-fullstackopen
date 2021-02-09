f4605c3dc0a2a500a6708bb66d84e52bced2a1f8
Felix Redondo <frmanzanares@hotmail.com>
2021-02-09 01:43:10 +0100

    Ejercicio 1.7 Se añaden nuevas estadísticas

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,6 +8,10 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
 
+	const total = good + neutral + bad
+	const positive = (good / total) * 100
+	const average = (good - bad) / total
+
 	return (
 		<div>
 			<div>
@@ -19,9 +23,12 @@ const App = () => {
 			<br/>
 			<div>
 				<h1>Statistics</h1>
-				<p><span>Good {good}</span></p>
-				<p><span>Neutral {neutral}</span></p>
-				<p><span>Bad {bad}</span></p>
+				<p><span>Good: {good}</span></p>
+				<p><span>Neutral: {neutral}</span></p>
+				<p><span>Bad: {bad}</span></p>
+				<p><span>All: {total}</span></p>
+				<p><span>Average: {average}</span></p>
+				<p><span>Positive: {positive} %</span></p>
 			</div>
 		</div>
 	)
