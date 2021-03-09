1f00fa1b148a3abf15d40da89b222bd4106ab6c1
Felix Redondo <frmanzanares@hotmail.com>
2021-03-09 01:55:05 +0100

    Ejercicio 2.4 Se extiende la aplicación para permitir un número arbitrario de cursos

--- part2/courseinfo2/src/index.js
+++ part2/courseinfo2/src/index.js
@@ -22,7 +22,6 @@ const Content = ({ parts }) => {
     return (
         <div>
             {parts.map(part => <Part key={part.id} part={part} />)}
-            <Total parts={parts} />
         </div>
     )
 }
@@ -32,40 +31,68 @@ const Course = ({course}) => {
         <div>
             <Header name={course.name} />
             <Content parts={course.parts} />
-            {/*<Total course={course} />*/}
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+const Courses = ({courses}) => {
+    return (
+        <div>
+            <Header name={"Web development curriculum"} />
+            {courses.map(course => <Course key={course.id} course={course} />)}
         </div>
     )
 }
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: 'Redux',
-                exercises: 11,
-                id: 4
-            }
-        ]
-    }
 
-    return <Course course={course} />
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+
+    return <Courses courses={courses} />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
