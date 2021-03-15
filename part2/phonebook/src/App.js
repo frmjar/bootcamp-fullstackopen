7eacbaeec43faebfa7d25bc8511cf4a74fb9b7db
Felix Redondo <frmanzanares@hotmail.com>
2021-03-15 13:19:39 +0100

    Ejercicio 2.11 Se inicializa la agenda haciendo una petición a un servidor json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import {PersonForm} from './components/PersonForm.js';
 import {Persons} from './components/Persons.js';
 import {Filter} from './components/Filter.js';
@@ -6,16 +7,17 @@ import './App.css';
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-123456'},
-    {name: 'Ada Lovelace', number: '39-44-5323523'},
-    {name: 'Dan Abramov', number: '12-43-234345'},
-    {name: 'Mary Poppendieck', number: '39-23-6423122'},
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setNewFilter] = useState('');
+
+  useEffect(() => {
+        axios.get('http://localhost:3001/persons')
+             .then(response => setPersons(response.data))
+             .catch(e => console.error('Error en el GET:', e));
+      }
+      , []);
 
   return (
       <div className={'container'}>
