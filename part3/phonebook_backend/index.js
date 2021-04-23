cb7429e908d231bae19270d47be56ecf787cbb83
Felix Redondo <frmanzanares@hotmail.com>
2021-04-23 22:42:54 +0200

    Ejercicio 3.15 Los contactos borrados se reflejan en la BBDD

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -18,33 +18,7 @@ morgan.token('data', (req) => {
 app.use(morgan(
     ':method :url :status :res[content-length] - :response-time ms :data'));
 
-let persons = [
-  {
-    'name': 'Arto Hellas',
-    'number': '040-123456',
-    'id': 1,
-  },
-  {
-    'name': 'Ada Lovelace',
-    'number': '39-44-5323523',
-    'id': 2,
-  },
-  {
-    'name': 'Dan Abramov',
-    'number': '12-43-234345',
-    'id': 3,
-  },
-  {
-    'name': 'Mary Poppendieck',
-    'number': '39-23-6423122',
-    'id': 4,
-  },
-  {
-    'name': 'hola',
-    'number': '39-23-6423122',
-    'id': 5,
-  },
-];
+let persons = [];
 
 app.get('/info', (request, response) => {
   const date = new Date();
@@ -95,9 +69,12 @@ app.get('/api/persons/:id', (request, response) => {
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const aux = persons.filter(person => person.id !== +request.params.id);
-  persons = [...aux];
-  response.status(204).end();
+  Person.deleteOne({'_id': request.params.id})
+        .then(() => response.status(204).end())
+        .catch(err => {
+          console.error(err);
+          response.status(500).send('Error al borrar persona').end();
+        });
 });
 
 const PORT = process.env.PORT || 3001;
