c8fc531d5c083d52cdb83f501c3bc3652744fbcd
Felix Redondo <frmanzanares@hotmail.com>
2021-07-12 15:35:28 +0200

    Ejercicio 4.19 Solo un usuario autentificado puede agregar blog nuevos. El usuario autentificado por el token sera el propietario el blog

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,14 +1,25 @@
 const blogRoutes = require('express').Router()
 const _ = require('lodash')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../utils/config')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const extractToken = (token) => _.isNil(token) ? null : token.substring(7)
+
 blogRoutes.get('/', async (request, response) => {
-  const blogs = await Blog.find().populate('user')
+  const blogs = await Blog.find().populate('user', { _id: 1, username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogRoutes.post('/', async (request, response) => {
+  const token = extractToken(request.get('authorization'))
+  const tokenDesencrypted = jwt.verify(token, JWT_SECRET)
+
+  if (!tokenDesencrypted.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
   const blog = new Blog(request.body)
   if (_.isUndefined(blog.title) || _.isUndefined(blog.url)) {
     return response.status(400).json({ error: 'title or url missing' })
@@ -16,8 +27,8 @@ blogRoutes.post('/', async (request, response) => {
 
   if (_.isUndefined(blog.likes)) { blog.likes = 0 }
 
-  const user = await User.findOne()
-  blog.user = user._id
+  const user = await User.findOne({ _id: tokenDesencrypted.id })
+  blog.user = tokenDesencrypted.id
 
   const result = await blog.save()
   user.blogs = user.blogs.concat(result._id)
