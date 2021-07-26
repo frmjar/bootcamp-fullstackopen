801dcb84fd05d222e80f5e7a1f67ec2b8614c0ba
frmjar <frmanzanares@hotmail.com>
2021-07-26 13:10:56 +0200

    Ejercicio 5.7 Se añade un boton en los blogs para desplegar mas informacion

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(!visible)}> {visible ? 'hide' : 'show'}</button>
+      </div>
+      <div style={{ display: visible ? 'block' : 'none' }}>
+        <div>{blog.url}</div>
+        <div>{blog.likes}</div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
